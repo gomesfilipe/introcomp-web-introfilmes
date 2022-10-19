c832c45949f352ce9f9a6ccd7735efa488c7a311
gomesfilipe <filipejvmf@gmail.com>
2022-10-18 22:33:49 -0300

    Continuando componente Form

--- introfilmes/src/components/Form/index.js
+++ introfilmes/src/components/Form/index.js
@@ -1,28 +1,38 @@
 import { Stars } from '../Stars'
+import IconPhoto from '../../assets/icons/icon-photo.svg'
+import Image from 'next/image'
 import styles from './Form.module.css'
 
 export function Form() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.photo}>
+  // const []
   
+  return (
+    <form className={styles.container}>
+      <div className={styles.photo}>
+        <Image src={IconPhoto} width={100} height={100}/>
+        <input type="text" placeholder="Insira o link da capa" className={styles.link}/>
       </div>
 
       <div className={styles.data}> { /** dados */ }
         <div className={styles.dataUp}>
+          <input type="text" placeholder="Insira o nome" className={[styles.name, styles.myInput].join(' ')} />
+          <input type="text" placeholder="Insira o ano de criação" className={[styles.year, styles.myInput].join(' ')} />
           <Stars readOnly={false} />
         </div>
         
         <div className={styles.dataDown}>
-
+          <input   
+            placeholder="Insira uma breve sinopse" 
+            className={[styles.description, styles.myInput].join(' ')}
+          />
         </div>
 
-        <div>
-          <button>
+        <div className={styles.submitButton}>
+          <button type="submit">
             Salvar
           </button>
         </div>
       </div>
-    </div>
+    </form>
   )
 }
