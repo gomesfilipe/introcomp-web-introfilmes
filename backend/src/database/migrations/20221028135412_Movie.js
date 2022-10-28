11036e069e88f3f656b71c2ce8f1f530438d83c4
gomesfilipe <filipejvmf@gmail.com>
2022-10-28 11:55:30 -0300

    Configuração do banco de dados e operações da camada de dados

---
+++ backend/src/database/migrations/20221028135412_Movie.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('movies', function(table) {
+    table.increments()
+
+    table.string('name').notNullable()
+    table.string('description').notNullable()
+    table.integer('year').notNullable()
+    table.integer('evaluation').notNullable()
+    table.string('photo').notNullable()
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('movies')
+};
