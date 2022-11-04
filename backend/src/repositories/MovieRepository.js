2bcdf2a2f4c1e68c1fb18ff59efa1a182ef00eb0
gomesfilipe <filipejvmf@gmail.com>
2022-11-03 23:17:13 -0300

    Ajuste no get por ano

--- backend/src/repositories/MovieRepository.js
+++ backend/src/repositories/MovieRepository.js
@@ -20,7 +20,7 @@ module.exports = class movieRepository {
     const movies = await connection('movies')
       .where(query => {
         if(yearDown) query.where('year', '>=', yearDown)
-        if(yearUp) query.where('year', '<', yearUp)
+        if(yearUp) query.where('year', '<=', yearUp)
         if(evaluationDown || evaluationDown === 0) query.where('evaluation', '>=', evaluationDown)
         if(evaluationUp || evaluationUp === 0) query.where('evaluation', '<=', evaluationUp)
         if(name) query.where('name', 'like', `${name}%`)
